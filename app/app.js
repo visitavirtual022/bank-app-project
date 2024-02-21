e6f5068a5e1a081cc37ac1a29d4db8dffaa03021
juanda <juandacorreo@gmail.com>
2024-02-21 22:30:11 +0100

    add cors

--- app/app.js
+++ app/app.js
@@ -1,8 +1,10 @@
 const express = require('express')
+const cors = require('cors')
 const mysql = require('mysql2')
 require('dotenv').config()
 
 const app = express()
+app.use(cors())
 const port = 3000
 let db
 
